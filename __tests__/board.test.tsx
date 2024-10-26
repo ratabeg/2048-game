474492a6d875f768296ea8be3af1c5b56688b217
Raouf Atabeg <raouf.ilkay@gmail.com>
2024-10-26 13:09:50 -0400

    fix(tests): changed from vitest to jest to fix testing errors

--- __tests__/board.test.tsx
+++ __tests__/board.test.tsx
@@ -1,4 +1,4 @@
-import { describe, expect, it } from "vitest";
+// import { describe, expect, it } from "vitest";
 import Board from "@/components/board";
 import { render, screen } from "@testing-library/react";
 
